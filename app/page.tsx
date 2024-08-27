f5f4ccde0e7bfdce40ea34dced791bcd67b707d9
gandli <gandli@qq.com>
2024-08-27 17:12:52 +0800

    feat: 更新全局CSS文件和页面组件

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ const getMovies = unstable_cache(
     return await db.select().from(schema.movies)
   },
   ['movies'],
-  { revalidate: 1, tags: ['movies'] }
+  { revalidate: 3600, tags: ['movies'] }
 )
 export default async function Home() {
   const movies = await getMovies();
 
   return (
     <div className='h-screen m-10'>
-      <h1>Home</h1>
-      <ul>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Home</h1>
+      <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
         {movies.map((movie) => (
           <li key={movie.id}>{movie.title}</li>
         ))}
