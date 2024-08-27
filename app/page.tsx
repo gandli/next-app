947df656174c1cd3c025486089644ae9a2da848a
gandli <gandli@qq.com>
2024-08-27 16:24:35 +0800

    feat(db): refactor movie retrieval with caching in Next.js app

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
 // app/page.tsx
-
+import { unstable_cache } from 'next/cache'
 import * as schema from "@/db/schema";
 import { db } from "@/db/db";
 
+const getMovies = unstable_cache(
+  async () => {
+    return await db.select().from(schema.movies)
+  },
+  ['movies'],
+  { revalidate: 1, tags: ['movies'] }
+)
 export default async function Home() {
-  const result = await db.select().from(schema.movies);
+  const movies = await getMovies();
 
   return (
-    <>
+    <div className='h-screen m-10'>
       <h1>Home</h1>
       <ul>
-        {result.map((movie) => (
+        {movies.map((movie) => (
           <li key={movie.id}>{movie.title}</li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
